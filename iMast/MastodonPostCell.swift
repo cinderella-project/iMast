c4a25def5cf5cc01820ab73638e8f3ab105cd162
rinsuki <6533808+rinsuki@users.noreply.github.com>
2017-12-28 10:37:17 +0900

    iOS11で投稿テキストをタップして投稿詳細が開けない不具合を修正

--- iMast/MastodonPostCell.swift
+++ iMast/MastodonPostCell.swift
@@ -106,6 +106,8 @@ class MastodonPostCell: UITableViewCell, UITextViewDelegate {
         // -- タッチ周り --
         textView.textContainerInset = UIEdgeInsets(top: 0, left: 0, bottom: 2, right: 0)
         textView.textContainer.lineFragmentPadding = 0
+        textView.isUserInteractionEnabled = true
+        textView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.tapPost)))
         iconView.isUserInteractionEnabled = true
         iconView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.tapUser)))
         userView.isUserInteractionEnabled = true
