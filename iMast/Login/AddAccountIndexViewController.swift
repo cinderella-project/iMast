d4f33b1b73d3462fe782dfda01bc62afae8df259
rinsuki <428rinsuki+git@gmail.com>
2018-10-20 09:30:10 +0900

    fix error

--- iMast/Login/AddAccountIndexViewController.swift
+++ iMast/Login/AddAccountIndexViewController.swift
@@ -32,9 +32,9 @@ class AddAccountIndexViewController: UIViewController, UITextFieldDelegate {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "goProgress" {
             let nextView = segue.destination as! AddAccountProgressViewController
-            if let hostName = hostNameField.text, hostName.count > 0) {
+            if let hostName = hostNameField.text, hostName.count > 0 {
                 nextView.setHost(hostName)
-            } else if let hostName = hostNameField.placeholder, hostName.count > 0) {
+            } else if let hostName = hostNameField.placeholder, hostName.count > 0 {
                 nextView.setHost(hostName)
             } else {
                 alert(title: "エラー", message: "ホスト名を入力してください。")
