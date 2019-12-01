646fbeb9e282566faa4ecb9b0d86ac8d66c64bff
rinsuki <428rinsuki+git@gmail.com>
2019-12-01 17:32:19 +0900

    fix

--- Sources/iOS/App/R.generated.swift
+++ Sources/iOS/App/R.generated.swift
@@ -155,20 +155,12 @@ struct R: Rswift.Validatable {
     fileprivate init() {}
   }
 
-  /// This `R.file` struct is generated, and contains static references to 3 files.
+  /// This `R.file` struct is generated, and contains static references to 2 files.
   struct file {
-    /// Resource file `Podfile`.
-    static let podfile = Rswift.FileResource(bundle: R.hostingBundle, name: "Podfile", pathExtension: "")
     /// Resource file `Settings.bundle`.
     static let settingsBundle = Rswift.FileResource(bundle: R.hostingBundle, name: "Settings", pathExtension: "bundle")
     /// Resource file `emoji.json`.
     static let emojiJson = Rswift.FileResource(bundle: R.hostingBundle, name: "emoji", pathExtension: "json")
-
-    /// `bundle.url(forResource: "Podfile", withExtension: "")`
-    static func podfile(_: Void = ()) -> Foundation.URL? {
-      let fileResource = R.file.podfile
-      return fileResource.bundle.url(forResource: fileResource)
-    }
 
     /// `bundle.url(forResource: "Settings", withExtension: "bundle")`
     static func settingsBundle(_: Void = ()) -> Foundation.URL? {
