46caca632ee8c51a9c87a4ffe2e6b5d8a4f8553a
rinsuki <428rinsuki+git@gmail.com>
2024-02-08 12:16:24 +0900

    chore: Add @UserInfoProperty(_: string) macro

--- Sources/Core/Package/Sources/iMastPackageMacros/PackageMacro.swift
+++ Sources/Core/Package/Sources/iMastPackageMacros/PackageMacro.swift
@@ -3,31 +3,58 @@ import SwiftSyntax
 import SwiftSyntaxBuilder
 import SwiftSyntaxMacros
 
-/// Implementation of the `stringify` macro, which takes an expression
-/// of any type and produces a tuple containing the value of that expression
-/// and the source code that produced the value. For example
-///
-///     #stringify(x + y)
-///
-///  will expand to
-///
-///     (x + y, "x + y")
-public struct StringifyMacro: ExpressionMacro {
+public struct UserInfoPropertyMacro: AccessorMacro {
     public static func expansion(
-        of node: some FreestandingMacroExpansionSyntax,
+        of node: AttributeSyntax,
+        providingAccessorsOf decl: some DeclSyntaxProtocol,
         in context: some MacroExpansionContext
-    ) -> ExprSyntax {
-        guard let argument = node.argumentList.first?.expression else {
-            fatalError("compiler bug: the macro does not have any arguments")
+    ) throws -> [AccessorDeclSyntax] {
+        // extract key part
+        guard case .argumentList(let arguments) = node.arguments else {
+            // TODO: throw error like "arguments required"
+            return []
+        }
+        guard arguments.count == 1, let key = arguments.first else {
+            // TODO: throw error like "arguments count wrong"
+            return []
         }
 
-        return "(\(argument), \(literal: argument.description))"
+        // extract type part
+        guard let decl = decl.as(VariableDeclSyntax.self) else {
+            // TODO: throw error like "this macro should be attached to var decl"
+            return []
+        }
+        guard decl.bindings.count == 1, let binding = decl.bindings.first else {
+            // TODO: throw error like "bindings count wrong"
+            return []
+        }
+        guard let typeAnot = binding.typeAnnotation else {
+            // TODO: throw error like "type annotation is required"
+            return []
+        }
+        let optionalInnerType = typeAnot.type.cast(OptionalTypeSyntax.self).wrappedType
+        return [
+            """
+            get {
+                userInfo?[\(key)] as? \(optionalInnerType)
+            }
+            """,
+            """
+            set {
+                if let newValue {
+                    addUserInfoEntries(from: [\(key): newValue])
+                } else {
+                    userInfo?.removeValue(forKey: \(key))
+                }
+            }
+            """,
+        ]
     }
 }
 
 @main
 struct PackagePlugin: CompilerPlugin {
     let providingMacros: [Macro.Type] = [
-        StringifyMacro.self,
+        UserInfoPropertyMacro.self,
     ]
 }
--- Sources/Core/Package/Tests/PackageTests/PackageTests.swift
+++ Sources/Core/Package/Tests/PackageTests/PackageTests.swift
@@ -3,40 +3,52 @@ import SwiftSyntaxMacrosTestSupport
 import XCTest
 
 // Macro implementations build for the host, so the corresponding module is not available when cross-compiling. Cross-compiled tests may still make use of the macro itself in end-to-end tests.
-#if canImport(PackageMacros)
-import PackageMacros
+#if canImport(iMastPackageMacros)
+import iMastPackageMacros
 
 let testMacros: [String: Macro.Type] = [
-    "stringify": StringifyMacro.self,
+    "UserInfoProperty": UserInfoPropertyMacro.self,
 ]
 #endif
 
 final class PackageTests: XCTestCase {
     func testMacro() throws {
-        #if canImport(PackageMacros)
+        #if canImport(iMastPackageMacros)
         assertMacroExpansion(
             """
-            #stringify(a + b)
+            extension Some {
+                @UserInfoProperty("user.info.key") var hoge: String?
+                @UserInfoProperty("user.info.int") var fuga: Int?
+            }
             """,
             expandedSource: """
-            (a + b, "a + b")
+            extension Some {
+                var hoge: String? {
+                    get {
+                        userInfo? ["user.info.key"] as? String
+                    }
+                    set {
+                        if let newValue {
+                            addUserInfoEntries(from: ["user.info.key": newValue])
+                        } else {
+                            userInfo?.removeValue(forKey: "user.info.key")
+                        }
+                    }
+                }
+                var fuga: Int? {
+                    get {
+                        userInfo? ["user.info.int"] as? Int
+                    }
+                    set {
+                        if let newValue {
+                            addUserInfoEntries(from: ["user.info.int": newValue])
+                        } else {
+                            userInfo?.removeValue(forKey: "user.info.int")
+                        }
+                    }
+                }
+            }
             """,
-            macros: testMacros
-        )
-        #else
-        throw XCTSkip("macros are only supported when running tests for the host platform")
-        #endif
-    }
-
-    func testMacroWithStringLiteral() throws {
-        #if canImport(PackageMacros)
-        assertMacroExpansion(
-            #"""
-            #stringify("Hello, \(name)")
-            """#,
-            expandedSource: #"""
-            ("Hello, \(name)", #""Hello, \(name)""#)
-            """#,
             macros: testMacros
         )
         #else
