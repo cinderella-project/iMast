da6dbef69df3f906da650fc0fb1c3d421d6fb76f
rinsuki <428rinsuki+git@gmail.com>
2025-02-01 14:29:41 +0900

    chore: アップロード時のメモリ節約

--- Sources/iOS/Core/UploadableMedia.swift
+++ Sources/iOS/Core/UploadableMedia.swift
@@ -25,6 +25,57 @@ import Foundation
 import UIKit
 import Ikemen
 
+private func imageResizer(data: Data, newSize: Int, opaque: Bool) -> UIImage? {
+    let cgImageSourceOptions = [
+        kCGImageSourceShouldCache: false,
+        kCGImageSourceCreateThumbnailFromImageAlways: true,
+        kCGImageSourceThumbnailMaxPixelSize: newSize,
+        kCGImageSourceCreateThumbnailWithTransform: true, // これがないと EXIF の回転フラグが考慮されない
+    ] as CFDictionary
+    if let cgImageSource = CGImageSourceCreateWithData(data as CFData, cgImageSourceOptions) {
+        if let thumbnail = CGImageSourceCreateThumbnailAtIndex(cgImageSource, 0, cgImageSourceOptions) {
+            return UIImage(cgImage: thumbnail)
+        }
+    }
+
+    // なんかだめだったのでフォールバック
+
+    let newSizeFloat = CGFloat(newSize)
+    // 画像の縮小と再圧縮
+    guard let image = UIImage(data: data) else {
+        return nil
+    }
+    var width = image.size.width
+    var height = image.size.height
+    if width > height { // 横長
+        if image.size.width > newSizeFloat { // リサイズする必要がある
+            height /= (width / newSizeFloat)
+            width = CGFloat(newSize)
+        }
+    } else if width < height { // 縦長
+        if image.size.width > newSizeFloat { // リサイズする必要がある
+            width /= (height / newSizeFloat)
+            height = CGFloat(newSize)
+        }
+    } else { // 正方形
+        if width > newSizeFloat {
+            width = CGFloat(newSize)
+            height = CGFloat(newSize)
+        }
+    }
+    print(width, height)
+    let size = CGSize(width: floor(width), height: floor(height))
+    let renderer = UIGraphicsImageRenderer(size: size, format: UIGraphicsImageRendererFormat() ※ {
+        $0.scale = 1.0
+        $0.opaque = opaque
+        $0.preferredRange = .standard
+    })
+
+    return renderer.image { context in
+        image.draw(in: context.format.bounds)
+    }
+}
+
 public struct UploadableMedia {
     public init(format: UploadableMedia.MeidaFormat, data: Data, url: URL?, thumbnailImage: UIImage) {
         self.format = format
@@ -46,38 +97,9 @@ public struct UploadableMedia {
     public func toUploadableData() -> Data {
         let newSize = Defaults.autoResizeSize
         if newSize != 0, self.format == .jpeg || self.format == .png {
-            let newSizeFloat = CGFloat(newSize)
-            // 画像の縮小と再圧縮
-            guard let image = UIImage(data: self.data) else {
+            let result = imageResizer(data: data, newSize: newSize, opaque: format == .png)
+            guard let result else {
                 return self.data
-            }
-            var width = image.size.width
-            var height = image.size.height
-            if width > height { // 横長
-                if image.size.width > newSizeFloat { // リサイズする必要がある
-                    height /= (width / newSizeFloat)
-                    width = CGFloat(newSize)
-                }
-            } else if width < height { // 縦長
-                if image.size.width > newSizeFloat { // リサイズする必要がある
-                    width /= (height / newSizeFloat)
-                    height = CGFloat(newSize)
-                }
-            } else { // 正方形
-                if width > newSizeFloat {
-                    width = CGFloat(newSize)
-                    height = CGFloat(newSize)
-                }
-            }
-            print(width, height)
-            let size = CGSize(width: floor(width), height: floor(height))
-            let renderer = UIGraphicsImageRenderer(size: size, format: UIGraphicsImageRendererFormat() ※ {
-                $0.scale = 1.0
-                $0.opaque = format == .png
-                $0.preferredRange = .standard
-            })
-            let result = renderer.image { context in
-                image.draw(in: context.format.bounds)
             }
             return (self.format == .png ? result.pngData() : result.jpegData(compressionQuality: 1.0))!
         }
